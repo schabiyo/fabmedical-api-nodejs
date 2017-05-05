242ef7478bc0c09577516b9f546e889833dc8bae
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-04 22:56:29 -0400

    added docker image version

--- ci/tasks/build-rc.sh
+++ ci/tasks/build-rc.sh
@@ -5,9 +5,10 @@ set -e -x
 
 echo "Building a release candidate with version number:"
 
-cat api-version/number
+ls
+#cat api-version/number
 
-touch tag-out/rc_tag
-echo "1.0.1" >> tag-out/rc_tag
+#touch tag-out/rc_tag
+#echo "1.0.1" >> tag-out/rc_tag
 
 
