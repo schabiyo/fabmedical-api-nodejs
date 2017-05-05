6d5cc69babbadbb83ab233a849656f6259b4990a
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-05 11:40:18 -0400

    deployment in paas dev

--- ci/tasks/deploy-to-dev-paas.sh
+++ ci/tasks/deploy-to-dev-paas.sh
@@ -11,6 +11,10 @@ echo "Image version: "
 #touch tag-out/rc_tag
 #echo "1.0.1" >> tag-out/rc_tag
 ## Config the Docker Container
+# 1-Login to Azure using the az command line
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+az account set --subscription "$subscription_id"
 az appservice web config container update -n $server_prefix-api-nodejs -g ossdemo-paas \
     --docker-registry-server-password $acr_password \
     --docker-registry-server-user $acr_username \
