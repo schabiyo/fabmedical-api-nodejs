d4b8352b7c2f6763f1a80e5192ff3cb1f3fd15cc
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-13 19:41:14 -0400

    exposing port 3001

--- ci/tasks/deploy-to-dev-paas.sh
+++ ci/tasks/deploy-to-dev-paas.sh
@@ -20,3 +20,4 @@ az appservice web config container update -s dev -n $server_prefix-api-nodejs -g
     --docker-registry-server-url $acr_endpoint \
     --docker-custom-image-name $acr_endpoint/ossdemo/api-nodejs:$img_tag
 
+az webapp config appsettings set --setting PORT=3001 -g $paas_rg -n $server_prefix-api-nodejs
